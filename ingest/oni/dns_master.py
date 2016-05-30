192f7500d870cb3984544778ff7fb1c5dd0c2e76
elopezsa <everardo.lopez.sandoval@intel.com>
2016-05-30 12:10:20 -0500

    fix indentation problem

--- ingest/oni/dns_master.py
+++ ingest/oni/dns_master.py
@@ -24,20 +24,20 @@ class dns_ingest(object):
 		self._dsource='dns'
 
 		# valdiate configuration info.
-        conf_err_msg = "Please provide a valid '{0}' in the configuration file"
-        Util.validate_parameter(conf['collector_path'],conf_err_msg.format("collector_path"))
-        Util.validate_parameter(conf['queue_name'],conf_err_msg.format("queue_name"))
-        Util.validate_parameter(conf['pkt_num'],conf_err_msg.format("pkt_num"))
-        Util.validate_parameter(conf['pcap_split_staging'],conf_err_msg.format("pcap_split_staging"))
-        Util.validate_parameter(conf['time_to_wait'],conf_err_msg.format("time_to_wait"))
+        	conf_err_msg = "Please provide a valid '{0}' in the configuration file"
+        	Util.validate_parameter(conf['collector_path'],conf_err_msg.format("collector_path"))
+        	Util.validate_parameter(conf['queue_name'],conf_err_msg.format("queue_name"))
+        	Util.validate_parameter(conf['pkt_num'],conf_err_msg.format("pkt_num"))
+        	Util.validate_parameter(conf['pcap_split_staging'],conf_err_msg.format("pcap_split_staging"))
+        	Util.validate_parameter(conf['time_to_wait'],conf_err_msg.format("time_to_wait"))
 
 		# set configuration.
 		self._collector_path = conf['collector_path']
-        self._hdfs_root_path = "{0}/{1}".format(conf['huser'] , self._dsource)
-        self._time_to_wait = conf['time_to_wait']
+        	self._hdfs_root_path = "{0}/{1}".format(conf['huser'] , self._dsource)
+        	self._time_to_wait = conf['time_to_wait']
 		self._pkt_num = conf['pkt_num']
-        self._pcap_split_staging = conf['pcap_split_staging']
-        self._queue_name = conf['queue_name']
+        	self._pcap_split_staging = conf['pcap_split_staging']
+        	self._queue_name = conf['queue_name']
 
 	def start(self):
 
@@ -49,7 +49,7 @@ class dns_ingest(object):
 					if file.endswith('.pcap'):
 						print "New file: {0}".format(file)
 						file_full_path = os.path.join(currdir,file)
-                        self._process_pcap_file(file,file_full_path,self._hdfs_root_path)
+                        			self._process_pcap_file(file,file_full_path,self._hdfs_root_path)
 						print "file :{0} processed".format(file)
 			time.sleep(self._time_to_wait)
 
@@ -80,11 +80,11 @@ class dns_ingest(object):
 										
 					#send rabbitmq notificaion.
 					hadoop_pcap_file = "{0}/{1}".format(hdfs_path,file)
-                    Util.send_new_file_notification(hadoop_pcap_file,self._queue_name)
+                    			Util.send_new_file_notification(hadoop_pcap_file,self._queue_name)
 				
 		rm_big_file = "rm {0}".format(file_local_path)
-        print rm_big_file
-        subprocess.call(rm_big_file,shell=True)
+        	print rm_big_file
+        	subprocess.call(rm_big_file,shell=True)
 		
 
 
